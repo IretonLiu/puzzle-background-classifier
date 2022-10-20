45e55c5950419988ade72b90cc4e9baf1eeef945
William Hill <whill4@me.com>
2022-10-20 16:57:18 +0200

    finished unet implementation

--- src/main.py
+++ src/main.py
@@ -11,52 +11,62 @@ from scipy.stats import multivariate_normal
 from ellipsoid import get_cov_ellipsoid
 from sklearn.mixture import GaussianMixture
 from classifier import Classifier
+from unet import UNet
+import torch
 
 res = (1024, 768)
 
 
 def read_data():
     # glob all images from data
-    image_files = glob.glob('data/images-1024x768/*.png')
-    mask_files = glob.glob('data/masks-1024x768/*.png')
+    image_files = glob.glob("data/images-1024x768/*.png")
+    mask_files = glob.glob("data/masks-1024x768/*.png")
 
     # convert to images using cv2 and convert to float RGB
-    images = np.array([cv2.cvtColor(cv2.imread(image), cv2.COLOR_BGR2RGB)
-                       for image in image_files])
-    masks = np.array([cv2.cvtColor(cv2.imread(mask), cv2.COLOR_BGR2GRAY) > 0
-                      for mask in mask_files])
+    images = np.array(
+        [cv2.cvtColor(cv2.imread(image), cv2.COLOR_BGR2RGB) for image in image_files]
+    )
+    masks = np.array(
+        [cv2.cvtColor(cv2.imread(mask), cv2.COLOR_BGR2GRAY) > 0 for mask in mask_files]
+    )
 
     return images, masks
 
 
 def split_dataset(images, masks):
     # split the images and masks into training validation and test with ration 70:15:15
-    training_images = images[:int(len(images) * 0.7)]
-    validation_images = images[int(len(images) * 0.7):int(len(images) * 0.85)]
-    testing_images = images[int(len(images) * 0.85):]
+    training_images = images[: int(len(images) * 0.7)]
+    validation_images = images[int(len(images) * 0.7) : int(len(images) * 0.85)]
+    testing_images = images[int(len(images) * 0.85) :]
 
-    training_masks = masks[:int(len(masks) * 0.7)]
-    validation_masks = masks[int(len(masks) * 0.7):int(len(masks) * 0.85)]
-    testing_masks = masks[int(len(masks) * 0.85):]
+    training_masks = masks[: int(len(masks) * 0.7)]
+    validation_masks = masks[int(len(masks) * 0.7) : int(len(masks) * 0.85)]
+    testing_masks = masks[int(len(masks) * 0.85) :]
 
-    return training_images, validation_images, testing_images, training_masks, validation_masks, testing_masks
+    return (
+        training_images,
+        validation_images,
+        testing_images,
+        training_masks,
+        validation_masks,
+        testing_masks,
+    )
 
 
 def scatter_plot(data, step, gmm=None):
     # 3d scatter plot of train_data_foreground
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(
-        data[::step, 0], data[::step, 1], data[::step, 2])
+    ax = fig.add_subplot(111, projection="3d")
+    ax.scatter(data[::step, 0], data[::step, 1], data[::step, 2])
 
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
+    ax.set_xlabel("X Label")
+    ax.set_ylabel("Y Label")
+    ax.set_zlabel("Z Label")
     if gmm:
         for j in range(gmm.n_components):
             # plot the gaussian
             x, y, z = get_cov_ellipsoid(gmm.covariances_[j], gmm.means_[j])
-            ax.plot_wireframe(x, y, z, color='r', alpha=0.1)
+            ax.plot_wireframe(x, y, z, color="r", alpha=0.1)
     plt.show()
 
 
@@ -65,41 +75,59 @@ def to_feature_vector(images, feature_type):
     image_dim = images[0].shape
     # convert images to feature vectors
     float_images = images.astype(np.float32) / 255.0
-    if feature_type == 'rgb':
+    if feature_type == "rgb":
         # flatten the images
         return float_images.reshape(-1, 3)
-    elif feature_type == 'hsv':
+    elif feature_type == "hsv":
         # convert float images to hsv
-        hsv = np.array([cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-                       for image in images]).reshape(-1, 3)
+        hsv = np.array(
+            [cv2.cvtColor(image, cv2.COLOR_RGB2HSV) for image in images]
+        ).reshape(-1, 3)
         # normalize the hsv values
         return hsv.astype(np.float32) / np.array([179, 255, 255])
 
-    elif feature_type == 'rgb+dog':
-        dog = np.array([cv2.GaussianBlur(image, (3, 3), 0) -
-                       cv2.GaussianBlur(image, (5, 5), 0) for image in float_images])
-        dog = np.array([(d - np.min(d))/(np.max(d) - np.min(d)) for d in dog])
+    elif feature_type == "rgb+dog":
+        dog = np.array(
+            [
+                cv2.GaussianBlur(image, (3, 3), 0) - cv2.GaussianBlur(image, (5, 5), 0)
+                for image in float_images
+            ]
+        )
+        dog = np.array([(d - np.min(d)) / (np.max(d) - np.min(d)) for d in dog])
         print(dog)
         return np.hstack((float_images.reshape(-1, 3), dog.reshape(-1, 3)))
-    elif feature_type == 'hsv+xy':
+    elif feature_type == "hsv+xy":
         # convert float images to hsv
-        hsv = np.array([cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-                       for image in images]).reshape(-1, 3)
+        hsv = np.array(
+            [cv2.cvtColor(image, cv2.COLOR_RGB2HSV) for image in images]
+        ).reshape(-1, 3)
         # a np array of the x and y coordinates
-        xy = np.array([np.indices(image_dim[:2]).transpose((1, 2, 0))
-                      for image in images]).reshape(-1, 2)
+        xy = np.array(
+            [np.indices(image_dim[:2]).transpose((1, 2, 0)) for image in images]
+        ).reshape(-1, 2)
         # normalize the hsv values
-        return np.hstack((hsv.astype(np.float32) / np.array([179, 255, 255]), xy.astype(np.float32) / np.array(image_dim[:2])))
+        return np.hstack(
+            (
+                hsv.astype(np.float32) / np.array([179, 255, 255]),
+                xy.astype(np.float32) / np.array(image_dim[:2]),
+            )
+        )
 
     else:
-        raise ValueError('Unknown feature type')
+        raise ValueError("Unknown feature type")
 
 
 def run_gmm():
 
     images, masks = read_data()
-    train_images, val_images, test_images, train_masks, val_masks, test_masks = split_dataset(
-        images, masks)
+    (
+        train_images,
+        val_images,
+        test_images,
+        train_masks,
+        val_masks,
+        test_masks,
+    ) = split_dataset(images, masks)
 
     # convert to pixels
     # train_data = np.concatenate([image.reshape(-1, 3)
@@ -126,31 +154,36 @@ def run_gmm():
                 print("Training background GMM with h = {}".format(background_h))
                 start_time = time.time()
                 gmm_background = GaussianMixtureModel(
-                    background_h, train_data_background.shape[1], max_iter=500, seed=4)
+                    background_h, train_data_background.shape[1], max_iter=500, seed=4
+                )
                 try:
                     gmm_background.fit(train_data_background)
                 except:
-                    print("Failed to fit background GMM with h = {}".format(
-                        background_h))
+                    print(
+                        "Failed to fit background GMM with h = {}".format(background_h)
+                    )
                     continue
-                print("Training time:", (time.time()-start_time))
+                print("Training time:", (time.time() - start_time))
                 gmm_background.save_model(
-                    f"models/gmm/{feature}/background/{background_h}/")
+                    f"models/gmm/{feature}/background/{background_h}/"
+                )
                 print(flush=True, end="")
 
             for foreground_h in foreground_h_list:
                 print("Training foreground GMM with h = {}".format(foreground_h))
                 start_time = time.time()
                 gmm_foreground = GaussianMixtureModel(
-                    foreground_h, train_data_foreground.shape[1], max_iter=500, seed=2)
+                    foreground_h, train_data_foreground.shape[1], max_iter=500, seed=2
+                )
                 try:
                     gmm_foreground.fit(train_data_foreground)
                 except:
                     print("Failed to fit foreground GMM with h = {}".format())
                     continue
-                print("Training time:", (time.time()-start_time))
+                print("Training time:", (time.time() - start_time))
                 gmm_foreground.save_model(
-                    f"models/gmm/{feature}/foreground/{foreground_h}/")
+                    f"models/gmm/{feature}/foreground/{foreground_h}/"
+                )
                 print(flush=True, end="")
             print("=============================================================")
 
@@ -169,30 +202,35 @@ def run_gmm():
             train_data_masks = train_masks.reshape(-1, 1)
             train_data_foreground = train_data[train_data_masks[:, 0]]
 
-            plt.imshow(
-                train_data_foreground[:6615184, :].reshape(2572, 2572, 3))
+            plt.imshow(train_data_foreground[:6615184, :].reshape(2572, 2572, 3))
             plt.show()
             train_data_background = train_data[~train_data_masks[:, 0]]
-            plt.imshow(
-                train_data_background[:19333609, :].reshape(4397, 4397, 3))
+            plt.imshow(train_data_background[:19333609, :].reshape(4397, 4397, 3))
             plt.show()
             classifier = Classifier(train_data, train_data_masks)
 
             for foreground_h in foreground_h_list:
 
                 gmm_foreground = GaussianMixtureModel(
-                    foreground_h, train_data_foreground.shape[1], max_iter=500, seed=2)
+                    foreground_h, train_data_foreground.shape[1], max_iter=500, seed=2
+                )
                 gmm_foreground.load_model(
-                    f"models/gmm/{feature}/foreground/{foreground_h}/")
+                    f"models/gmm/{feature}/foreground/{foreground_h}/"
+                )
 
                 scatter_plot(train_data_foreground, 10000, gmm_foreground)
                 for background_h in background_h_list:
 
                     gmm_background = GaussianMixtureModel(
-                        background_h, train_data_background.shape[1], max_iter=500, seed=4)
+                        background_h,
+                        train_data_background.shape[1],
+                        max_iter=500,
+                        seed=4,
+                    )
 
                     gmm_background.load_model(
-                        f"models/gmm/{feature}/background/{background_h}/")
+                        f"models/gmm/{feature}/background/{background_h}/"
+                    )
 
                     scatter_plot(train_data_background, 1000, gmm_background)
 
@@ -220,8 +258,21 @@ def run_gmm():
 
 
 def run_unet():
+    images, masks = read_data()
+    (
+        train_images,
+        val_images,
+        test_images,
+        train_masks,
+        val_masks,
+        test_masks,
+    ) = split_dataset(images, masks)
+
+    unet = UNet()
+    in_image = np.rollaxis(train_images[0], 2)
+    out = unet(torch.from_numpy(in_image))
 
 
 if __name__ == "__main__":
-    run_gmm()
+    # run_gmm()
     run_unet()
--- src/unet.py
+++ src/unet.py
@@ -1,8 +1,14 @@
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision.transforms.functional import crop
+import torch
 
 
 class DoubleConvolution(nn.Module):
+    """
+    perform two ReLU-activated 3x3 convolutions, where only the first will change the number of channels in the feature map
+    """
+
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.conv = nn.Sequential(
@@ -18,25 +24,74 @@ class DoubleConvolution(nn.Module):
 
 
 class Down(nn.Module):
+    """
+    this is one of a repeated "unit" which performs a double convolution, and then downsamples (for the contracting path).
+
+    convolution will double the number of channels, and is done with a 3x3 filter
+    max-pooling is done with 2x2 filter and stride 2 to reduce the dimensions of the feature map
+    """
+
     def __init__(self, in_channels, out_channels):
+        # assert in_channels * 2 == out_channels
         super().__init__()
-        self.mpconv = nn.Sequential(
-            nn.MaxPool2d(2, 2),
-            DoubleConvolution(in_channels, out_channels)
+        self.max_pool = nn.Sequential(
+            DoubleConvolution(in_channels, out_channels), nn.MaxPool2d(2, 2)
         )
 
     def forward(self, x):
-        x = self.mpconv(x)
+        # apply the downsampling
+        x = self.max_pool(x)
         return x
 
 
 class Up(nn.Module):
+    """
+    this is one of a repeated "unit" which performs upsampling and then convolution (for the expansive path)
+
+    upsample using transposed convolution with a 2x2 filter -> this also halves the number of channels
+    perform a double convolution with 3x3 filters
+    """
 
     def __init__(self, in_channels, out_channels):
+        # assert in_channels // 2 == out_channels
         super().__init__()
-        self.up = nn.ConvTranspose2d(
-            in_channels, in_channels // 2, 2, stride=2)
-        self.conv = DoubleConvolution(in_channels, out_channels)
+
+        # deconvolution with 2x2 filter and stride of 2 (is the green arrow)
+        self.upsample = nn.ConvTranspose2d(
+            in_channels, out_channels, kernel_size=2, stride=2
+        )
+
+        # perform the double convolution
+        self.double_convolution = DoubleConvolution(in_channels, out_channels)
+
+    def forward(self, x, feature_map):
+        # apply the upsampling, which will halve the number of channels so it matches the feature_map
+        x = self.upsample(x)
+
+        # crop the feature map so it has the same spatial dimensions as the current data
+        # this is necessary because the convolutions are not padded, so the size decreases
+        # get the size difference
+        vert_dim = 2
+        hori_dim = 3
+        dim0_size_diff = feature_map.size()[vert_dim] - x.size()[vert_dim]
+        dim1_size_diff = feature_map.size()[hori_dim] - x.size()[hori_dim]
+
+        # crop the feature map
+        feature_map = crop(
+            feature_map,
+            dim0_size_diff // 2,  # top left vertical component
+            dim1_size_diff // 2,  # top left horizontal component
+            x.size()[vert_dim],  # height of the cropped area
+            x.size()[hori_dim],  # width of the cropped area
+        )
+
+        # concatenate with the feature map from the corresponding downsampling step
+        x = torch.cat([x, feature_map], dim=1)
+
+        # apply the double convolution
+        x = self.double_convolution(x)
+
+        return x
 
 
 class UNet(nn.Module):
@@ -49,9 +104,43 @@ class UNet(nn.Module):
         super().__init__()
 
         # Contracting path
+        self.down1 = Down(3, 64)
+        self.down2 = Down(64, 128)
+        self.down3 = Down(128, 256)
+        self.down4 = Down(256, 512)
 
-        self.conv1 = DoubleConvolution(3, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
-        self.down4 = Down(512, 1024)
+        # Middle -> is the same as the Down layers, but without the final downsampling
+        self.middle = DoubleConvolution(512, 1024)
+
+        # Expansive path
+        # the number in the name of the layer matches to the down units for the concat of the feature map
+        self.up4 = Up(1024, 512)
+        self.up3 = Up(512, 256)
+        self.up2 = Up(256, 128)
+        self.up1 = Up(128, 64)
+
+        # final layer to reduce to a single dimension, which will be the classification
+        # map to 2 channels since there are 2 classes
+        self.final = nn.Conv2d(64, 2, kernel_size=1)
+
+    def forward(self, x):
+        # pass the image through the unet
+
+        # Contracting path
+        down1 = self.down1(x)
+        down2 = self.down2(down1)
+        down3 = self.down3(down2)
+        down4 = self.down4(down3)
+
+        # middle
+        middle = self.middle(down4)
+
+        # Expansive path
+        # the number in the name of the layer matches to the down units for the concat of the feature map
+        x = self.up4(middle, down4)
+        x = self.up3(x, down3)
+        x = self.up2(x, down2)
+        x = self.up1(x, down1)
+
+        # final layer to get the desired number of classes
+        return self.final(x)
